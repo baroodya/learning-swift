e71bc532ec5898a286649279b2c58a72f096f930
Alexander Baroody <abaroody@yext.com>
2022-07-03 19:48:41 -0400

    add swift project

---
+++ learning swift/learning swift/learning_swiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  learning_swiftApp.swift
+//  learning swift
+//
+//  Created by Alexander Baroody on 7/3/22.
+//
+
+import SwiftUI
+
+@main
+struct learning_swiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
